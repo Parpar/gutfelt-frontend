f64cb6af7e9f4c05baaa61a5cd086b8a0a2f3520
Parham Farahani <pf@gutfelt.com>
2025-09-04 10:15:08 +0200

    Final fix for login flow and build errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { UserProvider, UserContext } from './UserContext';
 
-// Importer KUN de komponenter, vi med sikkerhed skal bruge på øverste niveau
 import Header from './components/Header';
 import HomePage from './pages/homepage.js';
-
-// Importer siderne direkte, da vi ikke længere bruger ProtectedRoute
+import LoginPage from './pages/loginpage.js'; 
 import DocumentsPage from './pages/documentspage.js';
 import PolicyPage from './pages/policypage.js';
 import EmployeesPage from './pages/employeespage.js';
@@ -16,49 +14,65 @@ import PersonalePage from './pages/personale.js';
 import FaktureringPage from './pages/fakturering.js';
 import KundehåndteringPage from './pages/kundehåndtering.js';
 
-// Denne komponent indeholder nu den fulde, indloggede app-oplevelse
-function AppContent() {
-  const { currentUser, login } = useContext(UserContext);
-
-  // Den automatiske login-logik er den samme
-  useEffect(() => {
-    if (!currentUser) {
-      const autoLoginUser = { name: 'Susanne Nielsen', role: 'HR-redaktør' };
-      login(autoLoginUser);
-    }
-  }, [currentUser, login]);
-
-  // Viser en load-besked, indtil brugeren er automatisk logget ind
+// Denne komponent er nu i brug og vil ikke give fejl
+function ProtectedRoute({ children }) {
+  const { currentUser } = useContext(UserContext);
   if (!currentUser) {
-    return <div>Logger ind...</div>;
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+}
+
+// Vi laver en smart komponent, der vælger hvilke sider, der skal vises
+function AppRoutes() {
+  const { currentUser } = useContext(UserContext);
+
+  // Hvis ingen er logget ind, vis KUN login-siden
+  if (!currentUser) {
+    return (
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* Alle andre stier omdirigeres til login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    );
   }
 
-  // Når brugeren er logget ind, vises den fulde app
+  // Hvis en bruger ER logget ind, vis alle de beskyttede sider
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/standarder" element={<DocumentsPage />} />
+      <Route path="/firmapolitikker" element={<PolicyPage />} />
+      <Route path="/medarbejdere" element={<EmployeesPage />} />
+      <Route path="/samarbejdere" element={<PartnersPage />} />
+      <Route path="/firmapolitikker/personale" element={<PersonalePage />} />
+      <Route path="/firmapolitikker/fakturering" element={<FaktureringPage />} />
+      <Route path="/firmapolitikker/kundehåndtering" element={<KundehåndteringPage />} />
+      
+      {/* Hvis en logget-ind bruger går til /login, send dem til forsiden */}
+      <Route path="/login" element={<Navigate to="/" replace />} />
+      {/* Alle andre ukendte stier sendes til forsiden */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
+
+// Denne komponent indeholder den overordnede struktur
+function AppContent() {
   return (
     <Router>
       <div className="app">
         <Header />
         <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/standarder" element={<DocumentsPage />} />
-            <Route path="/firmapolitikker" element={<PolicyPage />} />
-            <Route path="/medarbejdere" element={<EmployeesPage />} />
-            <Route path="/samarbejdspartnere" element={<PartnersPage />} />
-            <Route path="/firmapolitikker/personale" element={<PersonalePage />} />
-            <Route path="/firmapolitikker/fakturering" element={<FaktureringPage />} />
-            <Route path="/firmapolitikker/kundehåndtering" element={<KundehåndteringPage />} />
-            
-            {/* Omdirigerer alle andre stier til forsiden */}
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+          <AppRoutes />
         </main>
       </div>
     </Router>
   );
 }
 
-// Hoved-appen "wrapper" nu kun AppContent i UserProvider
+// Hoved-appen er den samme
 function App() {
   return (
     <UserProvider>

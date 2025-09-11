eed70736349ac03eed177b3daac3b1a825c60df3
Parham Farahani <pf@gutfelt.com>
2025-09-11 15:35:17 +0200

    Fix syntax errors in all remaining subpage files

--- src/pages/persondatapolitik.js
+++ src/pages/persondatapolitik.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import UploadPage from './UploadPage';
-function PersondatapolitikPage() { return <UploadPage pageTitle="Persondatapolitik" category="persondatapolitik" backLink="/firmapolitikker" backLinkText="Firmapolitikker" iconClass="fa-user-shield" />; }
+
+function PersondatapolitikPage() {
+    return (
+        <UploadPage 
+            pageTitle="Persondatapolitik"
+            category="persondatapolitik"
+            backLink="/firmapolitikker"
+            backLinkText="Firmapolitikker"
+            iconClass="fa-user-shield"
+        />
+    );
+}
 export default PersondatapolitikPage;
-```*   **`slettepolitik.js`**:
-```jsx
-import React from 'react';
-import UploadPage from './UploadPage';
-function SlettepolitikPage() { return <UploadPage pageTitle="Slettepolitik" category="slettepolitik" backLink="/firmapolitikker" backLinkText="Firmapolitikker" iconClass="fa-trash-alt" />; }
-export default SlettepolitikPage;
